9b4859b2211b963302947194ff628b842e23daea
santiago rodriguez <santi.16jr@gmail.com>
2024-02-08 10:45:20 -0300

    .

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -7,12 +7,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCeP1Mq1F2HqA2Sxuzr5p1CVDOfJyD0QQw",
-  authDomain: "applestorejrx.firebaseapp.com",
-  projectId: "applestorejrx",
-  storageBucket: "applestorejrx.appspot.com",
-  messagingSenderId: "710189571457",
-  appId: "1:710189571457:web:a17d59a7055c858b1ddc80"
+ //
 };
 
 // Initialize Firebase
