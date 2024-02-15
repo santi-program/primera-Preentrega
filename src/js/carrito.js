83af08feaf436a89a113e3c5d77b68e628a35e98
santiago rodriguez <santi.16jr@gmail.com>
2024-02-15 13:09:51 -0300

    correccion de alertas

--- src/js/carrito.js
+++ src/js/carrito.js
@@ -138,7 +138,6 @@ function showMessage(message, type = "success") {
     Toastify({
         text: message,
         duration: 3000,
-        destination: "https://github.com/apvarun/toastify-js",
         newWindow: true,
         close: true,
         gravity: "bottom", // `top` or `bottom`
